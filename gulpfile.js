1c47f9659905374cac1a6b2f98645c5136ac9665
Wojciech Smoliński <wojsmol@wp.pl>
2025-01-29 23:28:47 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,24 +9,32 @@ var gulp = require('gulp');
 //=======================================================
 // Include Our Plugins
 //=======================================================
-var sync        = require('browser-sync');
+var sync = require('browser-sync');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var rename = require('gulp-rename');
+
+// Dynamically import gulp-autoprefixer (ESM module)
+var prefixPromise = import('gulp-autoprefixer');
 
 //=======================================================
-// Include Our tasks
+// Include Our tasks (using dynamic import for .mjs files)
 //=======================================================
-const taskCompile = await import('./gulp-tasks/compile.mjs');
-var taskMove        = require('./gulp-tasks/move.js');
-var taskLint        = require('./gulp-tasks/lint.js');
-var taskCompress    = require('./gulp-tasks/compress.js');
-var taskStyleGuide  = require('./gulp-tasks/styleguide.js');
-var taskConcat      = require('./gulp-tasks/concat.js');
+const taskCompile = require('./gulp-tasks/compile.js');
+var taskMove = require('./gulp-tasks/move.js');
+var taskLint = require('./gulp-tasks/lint.js');
+var taskCompress = require('./gulp-tasks/compress.js');
+var taskStyleGuide = require('./gulp-tasks/styleguide.js');
+var taskConcat = require('./gulp-tasks/concat.js');
 
 //=======================================================
 // Compile Our Sass and JS
 //=======================================================
 
-gulp.task('compile:sass', function() {
-  return taskCompile.sass();
+gulp.task('compile:sass', async function() {
+  const prefix = (await prefixPromise).default;
+  return taskCompile.sass(prefix);
 });
 
 gulp.task('compile:js', function() {
@@ -47,11 +55,11 @@ gulp.task('compile', gulp.series('compile:sass', 'compile:js', 'move:js'));
 // Lint Sass and JavaScript
 //=======================================================
 
-gulp.task('lint:sass', function () {
+gulp.task('lint:sass', function() {
   return taskLint.sass();
 });
 
-gulp.task('lint:js', function () {
+gulp.task('lint:js', function() {
   return taskLint.js();
 });
 
@@ -74,34 +82,33 @@ gulp.task('styleguide', function() {
 //=======================================================
 // Concat all CSS files into a master bundle
 //=======================================================
-gulp.task('concat', function () {
+gulp.task('concat', function() {
   return taskConcat.css();
 });
 
 //=======================================================
-// Clean all directories
+// Clean all directories (dynamically load clean tasks)
 //=======================================================
-
 async function loadCleanTask() {
-  return import('./gulp-tasks/clean.mjs');
+  return import('./gulp-tasks/clean.js');
 }
 
-gulp.task('clean:styleguide', async function () {
+gulp.task('clean:styleguide', async function() {
   const taskClean = await loadCleanTask();
   return taskClean.styleguide();
 });
 
-gulp.task('clean:css', async function () {
+gulp.task('clean:css', async function() {
   const taskClean = await loadCleanTask();
   return taskClean.css();
 });
 
-gulp.task('clean:js', async function () {
+gulp.task('clean:js', async function() {
   const taskClean = await loadCleanTask();
   return taskClean.js();
 });
 
-gulp.task('clean:docs', async function () {
+gulp.task('clean:docs', async function() {
   const taskClean = await loadCleanTask();
   return taskClean.docs();
 });
